b876688d5a71064feb7cc405a1d25284adb593ad
201344033 <jsy2540@naver.com>
2016-09-28 21:39:48 +0900

    " 'homework' is finish"

--- A01.java
+++ A01.java
@@ -1,4 +1,27 @@
-print("Hello world")
-print("Tell your world")
-print("Tell his world")
-print("Tell her world")
+import java.util.*;
+class A01{
+	 int age;
+	 public static void main(String[] s) {
+	 new A01().start();
+	}
+	 void start()
+	 {
+	 	Scanner s=new Scanner(System.in);
+		System.out.print("테어난 년도를 입력하세여 :");
+		int year = s.nextInt();
+		this.age = 2016 - year + 1;
+		print();	
+		
+	 }
+	 void print()
+	 {
+	 
+	 	if(this.age < 20)
+			System.out.print("미성년자입니다.");
+			
+		else
+			System.out.print("미성년자가 아닙니다.");
+		}
+		
+	}
+
